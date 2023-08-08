fef73d64a7cf7fe2b1d6bb769675efb793fe8805
k_czech <kamczeh@gmail.com>
2023-08-08 22:48:29 +0200

    feat:added apollo client, graphql and first query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,22 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import SEO from '../next-seo.config'
 import { DefaultSeo } from 'next-seo'
 import { CartContextProvider } from '@/components/Cart/CartContext'
+import { ApolloProvider } from '@apollo/client'
+import { apolloClient } from '@/graphql/apolloClient'
 
 const client = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CartContextProvider>
-      <Layout>
-        <DefaultSeo {...SEO} />
-        <QueryClientProvider client={client}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </Layout>
-    </CartContextProvider>
+    <ApolloProvider client={apolloClient}>
+      <CartContextProvider>
+        <Layout>
+          <DefaultSeo {...SEO} />
+          <QueryClientProvider client={client}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </Layout>
+      </CartContextProvider>
+    </ApolloProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
-import { Main } from "@/components/Main";
+import { Main } from '@/components/Main'
+import { useQuery, gql } from '@apollo/client'
+
+const GET_ALL_PRODUCTS = gql`
+  query GetAllProducts {
+    products {
+      id
+      slug
+      price
+      name
+    }
+  }
+`
 
 const HomePage = () => {
+  const { loading, error, data } = useQuery(GET_ALL_PRODUCTS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
   return (
     <Main>
-      <p>Siema</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </Main>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
